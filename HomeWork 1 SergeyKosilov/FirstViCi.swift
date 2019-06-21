702e63d66bc4c461629552b4b988c14acd44ffe0
Sergey Kosilov <sergejkosilov@macbook-pro-sergej.local>
2019-06-21 20:43:09 +0300

    первый подгон - какой то косяк в отображении на большой модели с лэйблом...

---
+++ HomeWork 1 SergeyKosilov/FirstViCi.swift
@@ -0,0 +1,28 @@
+//
+//  FirstVC.swift
+//  HomeWork 1 SergeyKosilov
+//
+//  Created by Сергей Косилов on 21/06/2019.
+//  Copyright © 2019 Сергей Косилов. All rights reserved.
+//
+
+import UIKit
+
+class FirstViCi: UIViewController {
+
+    @IBOutlet weak var btnPodskazka: UIButton!
+    
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextFild: UITextField!
+    @IBOutlet weak var labelNonLog: UILabel!
+    @IBOutlet weak var labelNonPassword: UILabel!
+    @IBOutlet weak var btnEnterData: UIButton!
+    @IBOutlet weak var btnNonLog: UIButton!
+    
+    @IBOutlet weak var btnNonPass: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+}
