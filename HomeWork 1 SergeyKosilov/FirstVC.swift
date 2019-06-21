8a4aa2de8a41ffb68e4d974419fb5e69a951d311
Sergey Kosilov <sergejkosilov@macbook-pro-sergej.local>
2019-06-21 07:00:18 +0300

    Initial Commit

---
+++ HomeWork 1 SergeyKosilov/FirstVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWork 1 SergeyKosilov
+//
+//  Created by Сергей Косилов on 21/06/2019.
+//  Copyright © 2019 Сергей Косилов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+    }
+
+
+}
+
