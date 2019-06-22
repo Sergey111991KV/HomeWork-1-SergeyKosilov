5483bfff6e35f3c89fc117a6016075cb4e478d52
Sergey Kosilov <sergejkosilov@macbook-pro-sergej.local>
2019-06-22 07:56:14 +0300

    проблема с label, решил но не понравилось

--- HomeWork 1 SergeyKosilov/FirstViCi.swift
+++ HomeWork 1 SergeyKosilov/FirstViCi.swift
@@ -8,21 +8,77 @@
 
 import UIKit
 
-class FirstViCi: UIViewController {
+class FirstViCi: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var btnPodskazka: UIButton!
     
     @IBOutlet weak var loginTextField: UITextField!
+    
     @IBOutlet weak var passwordTextFild: UITextField!
+    
     @IBOutlet weak var labelNonLog: UILabel!
+    
     @IBOutlet weak var labelNonPassword: UILabel!
+    
     @IBOutlet weak var btnEnterData: UIButton!
+    
     @IBOutlet weak var btnNonLog: UIButton!
     
     @IBOutlet weak var btnNonPass: UIButton!
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    @IBOutlet weak var btnEnter: UIButton!
+    
+    
+    @IBAction func tapGestureRecognizer(_ sender: UITapGestureRecognizer) {
+        self.loginTextField.resignFirstResponder()
+        self.passwordTextFild.resignFirstResponder()
     }
     
+    
+    @IBAction func textFieldCganged(_ sender: UITextField) {
+        
+        let login = loginTextField.text
+        let password = passwordTextFild.text
+        if login != "Write Login", login != "", password != "Write Password", password != "" {
+            btnEnter.isEnabled = true
+            
+        }else{
+             btnEnter.isEnabled = false
+            
+        }
+
+        
+    }
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        btnEnter.isEnabled = false
+        loginTextField.delegate = self
+        passwordTextFild.delegate = self
+        labelNonPassword.textColor = #colorLiteral(red: 0.5186036825, green: 0.8912252784, blue: 0.9079722762, alpha: 1)
+        labelNonLog.textColor = #colorLiteral(red: 0.5186036825, green: 0.8912252784, blue: 0.9079722762, alpha: 1)
+    }
+    
+    
+   
+    
+    override func viewWillAppear(_ animated: Bool) {
+      
+      
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+
 
 }
+//1.Пытался сделать label.isEnable = false - пропадали textField
